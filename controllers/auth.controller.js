e0ea2e2de0f1d3172b33de599106585f50e412b9
Noah Dev <developerncz@gmail.com>
2023-09-04 22:40:08 -0600

    feat: :heavy_plus_sign: Agrega la dependencia para verificar el token de Google
    Crea helper para verificar token. Modifica el modelo de User para agregar un rol por defecto. Modificamos controlador para obtener al nuevo usuario logueado con Google.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import bcryptjs from 'bcryptjs';
-import { request, response } from 'express';
+import { json, request, response } from 'express';
 
 import User from '../models/user.model.js';
 import { generateJWT } from '../helpers/generate-jwt.js';
+import { googleVerify } from '../helpers/google-verify.js';
 
 const loginController = async (req = request, res = response) => {
     const { email, password } = req.body;
@@ -50,13 +51,50 @@ const loginController = async (req = request, res = response) => {
     }
 }
 
-const googleSignInController = (req = request, res = response) => {
+const googleSignInController = async (req = request, res = response) => {
     const { id_token } = req.body;
 
-    res.json({
-        message: 'Inicio de sesion correcto',
-        id_token
-    })
+    try {
+        const { name, picture, email } = await googleVerify(id_token);
+
+        let userFromGoogle = await User.findOne({ email });
+
+        // El usuario no existe
+        if (!userFromGoogle) {
+            const data = {
+                name,
+                email,
+                password: 'NO-PASS',
+                img: picture,
+                google: true
+            };
+
+            userFromGoogle = new User(data);
+            await userFromGoogle.save();
+        }
+
+        // Usuario habilitado en BD
+        if (!userFromGoogle.status) {
+            return res.status(401).json({
+                message: 'Usuario no habilitado. Comuniquese con el Admin'
+            });
+        }
+
+        // Generar JSON Web Token
+        const token = await generateJWT(userFromGoogle.id);
+
+        res.json({
+            userFromGoogle,
+            token
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            result: false,
+            message: 'El token no se pudo verificar'
+        });
+    }
+
 }
 
 export {
